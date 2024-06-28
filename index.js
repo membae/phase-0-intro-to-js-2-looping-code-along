55d0edfdab18f9a7ba75d9323011d7e0f41fa11f
membae <membaphilip1@gmail.com>
2024-06-28 13:23:45 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names,event){
+    let messages=[];
+    names = ["Guadalupe", "Ollie", "Aki"];
+    event = "surprise";
+    for(let i=0;i<names.length;i++){
+        let message =`Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(message);
+    }
+    return messages;
+}
+function countDown(number){
+    for(let i=number;i>=0;i--){
+        console.log(i);
+    }
+}
+countDown(10);
